de687a28c410ad1cfdd87f43bf9285ffba101a88
Szymon <7ckonczyk@gmail.com>
2021-11-16 16:15:04 +0100

    programySN

---
+++ QT/papierkamien/papierkamien/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    int komp, uzyt;
+    void wybierz(int);
+    void wygrana(int,int);
+
+private slots:
+    void on_u1_clicked();
+
+    void on_u2_clicked();
+
+    void on_u3_clicked();
+
+    void on_reset_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
