0d17a736ae86930d1ff7b78bed44ac9a30d68ff9
Szymon <7ckonczyk@gmail.com>
2021-12-08 09:41:29 +0100

    programySN

---
+++ cpp/dziedziczenie/2lekcja/nauka/naukaDziedziczenie/naukaDziedziczenie.cpp
@@ -0,0 +1,112 @@
+﻿// naukaDziedziczenie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class Pracownik {
+public:
+    string imie, nazwisko;
+    virtual void zwrocDane();
+};
+
+class Nauczyciel : public Pracownik {
+public:
+    string przedmiot;
+    void zwrocDane();
+};
+
+class Wychowawca : public Nauczyciel {
+public:
+    string klasa;
+    void zwrocDane();
+};
+
+int main()
+{
+    Pracownik* w_pracownik;
+
+    Pracownik p1;
+    w_pracownik = &p1;
+    w_pracownik->imie = "jan";
+    w_pracownik->nazwisko = "kowalski";
+    w_pracownik->zwrocDane();
+
+    Nauczyciel p2;
+    w_pracownik = &p2;
+    w_pracownik->imie = "Adam";
+    w_pracownik->nazwisko = "Nowak";
+    w_pracownik->zwrocDane();
+
+    Wychowawca p3;
+    w_pracownik = &p3;
+    w_pracownik->imie = "maria";
+    w_pracownik->nazwisko = "jakas";
+    w_pracownik->zwrocDane();
+}
+
+void Pracownik::zwrocDane()
+{
+    cout << "PRACOWNIIK" << endl;
+    cout << imie << " " << nazwisko << endl;
+}
+
+void Nauczyciel::zwrocDane()
+{
+    cout << "NAUCZYCIEL" << endl;
+    cout << imie << " " << nazwisko << " " << przedmiot << endl;
+}
+
+void Wychowawca::zwrocDane()
+{
+    cout << "WYCHOWAWCA" << endl;
+    cout << imie << " " << nazwisko << " " << przedmiot << " " << klasa << endl;
+}
+
+
+
+/*#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class A {
+public:
+    string a = "klasa A";
+    void wypisz() {
+        cout << a << endl;
+    }
+};
+
+class B : public A {
+public:
+    string b="klasa B";
+    void wypisz() {
+        cout << a << " " << b << endl;
+    }
+};
+
+class C : public B {
+public:
+    string c = "klasa C";
+    void wypisz() {
+        cout << a << " " << b << " " << c << endl;
+    }
+};
+
+int main()
+{
+    A a;
+    B b;
+    C c;
+    a.wypisz();
+    b.wypisz();
+    c.wypisz();
+}*/
+
+
+
+
---
+++ cpp/klasyAbstrakcyjne/zad1/zad1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+class Osoba {
+public:
+    string imie, nazwisko;
+    virtual void wyswietlDane() = 0;
+};
+
+class Lekarz : public Osoba {
+public:
+    string specjalizacja;
+    void wyswietlDane() {
+        cout << imie << " " << nazwisko << " " << specjalizacja << endl;
+    }
+};
+
+class Ordynator : public Lekarz {
+public:
+    string oddzial;
+    void wyswietlDane() {
+        cout << imie << " " << nazwisko << " " << specjalizacja << " " << oddzial << endl;
+    }
+};
+
+int main()
+{
+    Lekarz* p1 = new Lekarz;
+    p1->imie = "jan";
+    p1->nazwisko = "taki";
+    p1->specjalizacja = "kardiolog";
+    p1->wyswietlDane();
+
+    Ordynator* o1 = new Ordynator;
+    o1->imie = "jan";
+    o1->nazwisko = "taki";
+    o1->specjalizacja = "kardiolog";
+    o1->oddzial = "dzieciecy";
+    o1->wyswietlDane();
+}
+
